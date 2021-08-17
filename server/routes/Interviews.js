d849b28ac7cb9b301f54a2ab69a012e20573e584
99hash63 <thkottegoda@gmail.com>
2021-08-17 14:42:16 +0530

    formatted code with prettier extention

--- server/routes/Interviews.js
+++ server/routes/Interviews.js
@@ -1,38 +1,40 @@
-const router = require("express").Router();
-const { request } = require("express");
+const router = require('express').Router();
+const { request } = require('express');
 //import the interviews model//
-let Interviews = require("../models/Interviews");
+let Interviews = require('../models/Interviews');
 
-router.route("/add").post((req,res)=>{
-    //body
-    //const QNo = Number(req.body.QNo);
-    const JobTitle = req.body.JobTitle;
-    const QuestionType = req.body.QuestionType;
-    const Offer = req.body.Offer;
-    const Question = req.body.Question;
-    const Answer = req.body.Answer;
-    const User = req.body.User;
-    const SaveOp = "no"
+router.route('/add').post((req, res) => {
+	//body
+	//const QNo = Number(req.body.QNo);
+	const JobTitle = req.body.JobTitle;
+	const QuestionType = req.body.QuestionType;
+	const Offer = req.body.Offer;
+	const Question = req.body.Question;
+	const Answer = req.body.Answer;
+	const User = req.body.User;
+	const SaveOp = 'no';
 
-    const newQuestion = new Interviews({
-        //QNo,
-        JobTitle,
-        QuestionType,
-        Offer,
-        Question,
-        Answer,
-        User,
-        SaveOp
-    })
+	const newQuestion = new Interviews({
+		//QNo,
+		JobTitle,
+		QuestionType,
+		Offer,
+		Question,
+		Answer,
+		User,
+		SaveOp,
+	});
 
-    newQuestion.save().then(()=>{
-        //body
-        res.json("Question added successfully")
-    }).catch((err)=>{
-        console.log(err);
-    })
-
-})
+	newQuestion
+		.save()
+		.then(() => {
+			//body
+			res.json('Question added successfully');
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 //retirve
 
@@ -45,53 +47,61 @@ router.route("/add").post((req,res)=>{
     })
 })*/
 //retrive common interview question
-router.route("/displayci").get((req,res)=>{
-    //body
-    Interviews.find({QuestionType: 'Common Interview Question'}).then((questions)=>{
-        res.json(questions)
-    }).catch((err)=>{
-        console.log(err);
-    })
-})
+router.route('/displayci').get((req, res) => {
+	//body
+	Interviews.find({ QuestionType: 'Common Interview Question' })
+		.then((questions) => {
+			res.json(questions);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 //retrive interview guildlines
-router.route("/displayig").get((req,res)=>{
-    //body
-    Interviews.find({QuestionType: 'Interview Guidelines'}).then((questions)=>{
-        res.json(questions)
-    }).catch((err)=>{
-        console.log(err);
-    })
-})
+router.route('/displayig').get((req, res) => {
+	//body
+	Interviews.find({ QuestionType: 'Interview Guidelines' })
+		.then((questions) => {
+			res.json(questions);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 //retrive saved question list
-router.route("/displaysq").get((req,res)=>{
-    //body
-    Interviews.find({SaveOp: 'yes'}).then((questions)=>{
-        res.json(questions)
-    }).catch((err)=>{
-        console.log(err);
-    })
-})
+router.route('/displaysq').get((req, res) => {
+	//body
+	Interviews.find({ SaveOp: 'yes' })
+		.then((questions) => {
+			res.json(questions);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 //retrive users question pool
-router.route("/displayuserqp").get((req,res)=>{
-    //body
-    Interviews.find({User: 'Anuradha'}).then((questions)=>{
-        res.json(questions)
-    }).catch((err)=>{
-        console.log(err);
-    })
-})
+router.route('/displayuserqp').get((req, res) => {
+	//body
+	Interviews.find({ User: 'Anuradha' })
+		.then((questions) => {
+			res.json(questions);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 //update
-router.route("/update/:id").put(async(req,res)=>{
-    let qno = req.params.id;
-    //destructure
-    //const {JobTitle,QuestionType,Offer,Question,Answer,User} = req.body;
-    const {Question,Answer} = req.body;
+router.route('/update/:id').put(async (req, res) => {
+	let qno = req.params.id;
+	//destructure
+	//const {JobTitle,QuestionType,Offer,Question,Answer,User} = req.body;
+	const { Question, Answer } = req.body;
 
-    /*const updateInterview ={
+	/*const updateInterview ={
         JobTitle,
         QuestionType,
         Offer,
@@ -99,30 +109,38 @@ router.route("/update/:id").put(async(req,res)=>{
         Answer,
         User
     }*/
-    const updateInterview ={
-        Question,
-        Answer
-    }
+	const updateInterview = {
+		Question,
+		Answer,
+	};
 
-    const update = await Interviews.findByIdAndUpdate(qno,updateInterview).then(()=>{
-        res.status(200).send({status: "Successfully Updated" })
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).send({status: "Error with updating data", error: err.message});
-    })
-})
+	const update = await Interviews.findByIdAndUpdate(qno, updateInterview)
+		.then(() => {
+			res.status(200).send({ status: 'Successfully Updated' });
+		})
+		.catch((err) => {
+			console.log(err);
+			res
+				.status(500)
+				.send({ status: 'Error with updating data', error: err.message });
+		});
+});
 
 //delete
-router.route("/delete/:id").delete(async(req,res) => {
-    let qno = req.params.id;
+router.route('/delete/:id').delete(async (req, res) => {
+	let qno = req.params.id;
 
-    await Interviews.findByIdAndDelete(qno).then(()=>{
-        res.status(200).send({status : "Question deleted"});
-    }).catch((err)=>{
-        console.log(err.message);
-        res.status(500).send({status: "Error with delete data", error: err.message});
-    })
-})
+	await Interviews.findByIdAndDelete(qno)
+		.then(() => {
+			res.status(200).send({ status: 'Question deleted' });
+		})
+		.catch((err) => {
+			console.log(err.message);
+			res
+				.status(500)
+				.send({ status: 'Error with delete data', error: err.message });
+		});
+});
 
 //one ritrive
 /*router.route("/get/.user").get(async(req,res)=>{
@@ -135,7 +153,5 @@ router.route("/delete/:id").delete(async(req,res) => {
         res.status(500).send({status: "Error fetching", error: err.message});
     })
 })*/
-
-
 
 module.exports = router;
