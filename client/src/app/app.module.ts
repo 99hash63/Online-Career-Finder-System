785353969cff7a0dadb3a596aaf9bfd73357033e
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-08-18 07:32:28 +0530

    Frontend routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {Router,RouterModule, Routes} from '@angular/router'
+import { Router, RouterModule, Routes } from '@angular/router';
+import { ROUTES } from './app.routing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-const appRouter: Routes=[
-  { path:'', component: AppComponent}
-]
+import { CreateJobComponent } from './JOBS/create-job/create-job.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CreateJobComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRouter,{enableTracing:true})
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  hasRoute(route: String) {
+    return this.router.url === route;
   }
 
+  changeHeader() {
+    
+  }
 }
