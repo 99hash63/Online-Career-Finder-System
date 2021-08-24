cf7b0b0e2cfd9dcb06ec4a31cc62d28e4dd73faf
99hash63 <thkottegoda@gmail.com>
2021-08-24 15:46:23 +0530

    created register request from client to server

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from './user.service';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
   registerImg:string = "../../assets/images/signup-image.jpg";
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  resetForm(form?: NgForm){
+    if(form)
+      form.reset();
+      this.userService.selectedUser = {
+        _id!: "",
+        name!: "",
+        email!: "",
+        password!: "",
+        createdAt!: "",
+      }
+  }
+  onSubmit(form: NgForm) {
+    this.userService.postUser(form.value).subscribe((res)=>{
+      this.resetForm(form)
+    })
   }
 
 }
---
+++ client/src/app/register/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/map'
+// import 'rxjs/add/operator/toPromise'
+
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  selectedUser!: User;
+  user!: User[];
+  readonly baseURL = 'http://localhost:5000/api/v1/auth/register';
+
+
+  constructor(private http: HttpClient) { }
+
+  postUser(user : User){
+  return this.http.post(this.baseURL, user);
+  }
+}
