31a4c399c969cda3cf73a65f469d9858dd26acfb
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-08-27 15:58:05 +0530

    Recreated MyJobs UI

--- client/src/app/JOBS/my-jobs/my-jobs.component.ts
+++ client/src/app/JOBS/my-jobs/my-jobs.component.ts
@@ -1,20 +1,128 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+// import { Component, OnInit } from '@angular/core';
+// import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+// @Component({
+//   selector: 'app-my-jobs',
+//   templateUrl: './my-jobs.component.html',
+//   styleUrls: ['./my-jobs.component.css'],
+//   providers: [NgbModalConfig, NgbModal],
+// })
+// export class MyJobsComponent implements OnInit {
+//   constructor(config: NgbModalConfig, private modalService: NgbModal) {
+//     // customize default values of modals used by this component tree
+//     config.backdrop = 'static';
+//     config.keyboard = false;
+//   }
+
+//   ngOnInit(): void {}
+//   open(content: any) {
+//     this.modalService.open(content, {
+//       backdrop: 'static',
+//     });
+//   }
+// }
+
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { JobpostService } from '../jobpost.service';
+import { Jobpost } from '../jobpost.model';
+import { PageEvent } from '@angular/material/paginator';
+import { IndustryOptions } from '../../../assets/industries';
+import {
+  FormGroup,
+  FormBuilder,
+  FormGroupDirective,
+  NgForm,
+  FormControl,
+  Validators,
+  PatternValidator,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-my-jobs',
   templateUrl: './my-jobs.component.html',
   styleUrls: ['./my-jobs.component.css'],
-  providers: [NgbModalConfig, NgbModal],
+  encapsulation: ViewEncapsulation.None,
+  providers: [JobpostService, NgbModalConfig, NgbModal],
 })
 export class MyJobsComponent implements OnInit {
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+  closeResult!: string;
+  imageError?: string;
+  isImageSaved?: boolean;
+  cardImageBase64?: string;
+  datediff?: number;
+  searchTerm!: string;
+  industry!: string;
+  type: string = 'Type';
+  _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+  showJobPost: any = [];
+  totalLength!: number;
+  page!: number;
+  // MatPaginator Output
+  pageEvent!: PageEvent;
+
+  pageSize: number = 10;
+
+  JobType = new FormControl();
+  typeList: string[] = ['All', 'Full Time', 'Part Time', 'Internship'];
+
+  selectedTypes!: string;
+
+  IndustryOptions = IndustryOptions;
+
+  handlePageEvent(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.page = event.pageIndex;
+    this.totalLength = event.length;
+    console.log(this.page);
+  }
+
+  constructor(
+    private modalService: NgbModal,
+    public jobpostservice: JobpostService,
+    config: NgbModalConfig
+  ) {
     // customize default values of modals used by this component tree
     config.backdrop = 'static';
     config.keyboard = false;
   }
+  ngOnInit(): void {
+    this.refreshJobList();
+  }
+  setJob(job: Jobpost) {
+    this.jobpostservice.selectedJob = job;
+  }
 
-  ngOnInit(): void {}
+  openScrollableContent(longContent: any) {
+    this.modalService.open(longContent, {
+      size: 'lg',
+      backdrop: 'static',
+      scrollable: true,
+    });
+  }
+  refreshJobList() {
+    this.jobpostservice.getalljobs().subscribe((res) => {
+      this.jobpostservice.jobs = res as Jobpost[];
+
+      this.showJobPost = res;
+      this.totalLength = this.showJobPost.length;
+    });
+  }
+  calcDateDiff(date: any) {
+    var today = new Date();
+    var s = new Date(date);
+
+    var diff = Math.abs(today.getTime() - s.getTime());
+    var diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+    return diffDays + 'd';
+  }
+  achiring() {
+    if (this.jobpostservice.selectedJob.activelyHiring == 'Yes') {
+      return true;
+    }
+    return false;
+  }
   open(content: any) {
     this.modalService.open(content, {
       backdrop: 'static',
