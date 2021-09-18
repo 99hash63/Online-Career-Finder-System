e1c39ffd1384841426699cc9d4e6a3725ec699d5
99hash63 <thkottegoda@gmail.com>
2021-09-18 21:36:47 +0530

    modified company component with new coding structure

--- client/src/app/companies/companies.service.ts
+++ client/src/app/companies/companies.service.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@angular/core';
 import { Company } from './company.model';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-import { CompanyResponse } from './company-response.model';
 import { MyCompanyOverviewModel } from './company-overview.model';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class CompaniesService {
+  selectedCompany: Company = {
+    _id: '',
+    logo: '',
+    coverPhoto: '',
+    title: '',
+    industry: '',
+    emp_count: '',
+    description: '',
+  };
   constructor(private http: HttpClient, private router: Router) {}
 
-  private companies: Company[] = [];
+  public companies: Company[] = [];
   private myCompany: MyCompanyOverviewModel[] = [];
   private companiesUpdated = new Subject<Company[]>();
   getCompanies() {
@@ -28,10 +34,7 @@ export class CompaniesService {
     this.companies.push(company);
     this.companiesUpdated.next([...this.companies]);
 
-    return this.http.post<CompanyResponse>(
-      environment.apiBaseUrl + '/companies',
-      company
-    );
+    return this.http.post(environment.apiBaseUrl + '/companies', company);
   }
 
   getMyCompaniesDB() {
@@ -41,8 +44,8 @@ export class CompaniesService {
   getSingleCompany(_id: Object) {
     const newURL = environment.apiBaseUrl + '/companies/' + _id;
     // console.log(newURL);
-    this.http.get<CompanyResponse>(newURL).subscribe((res) => {
-      this.myCompany = res.data as MyCompanyOverviewModel[];
+    this.http.get(newURL).subscribe((res) => {
+      this.myCompany = res['data'] as MyCompanyOverviewModel[];
       // console.log(this.myCompany);
       this.router.navigate(['myCompanyOverview']);
     });
