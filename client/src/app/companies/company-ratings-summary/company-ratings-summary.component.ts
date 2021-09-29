232d3811306455fb056a1a7f4649bce16f0bcda9
99hash63 <thkottegoda@gmail.com>
2021-09-29 06:24:31 +0530

    created graph using chart js and it does not diplay

--- client/src/app/companies/company-ratings-summary/company-ratings-summary.component.ts
+++ client/src/app/companies/company-ratings-summary/company-ratings-summary.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CompanyRating } from '../company-rating.model';
 import { CompanyRatingsService } from '../company-ratings.service';
 import { CompaniesService } from '../companies.service';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-company-ratings-summary',
   templateUrl: './company-ratings-summary.component.html',
@@ -10,7 +10,7 @@ import { CompaniesService } from '../companies.service';
 })
 export class CompanyRatingsSummaryComponent implements OnInit {
   companyRatings: CompanyRating[] = [];
-
+  chart: any = [];
   //properties for display summary
   public totalReviews: number;
   public ratingsOverall: number;
@@ -30,6 +30,45 @@ export class CompanyRatingsSummaryComponent implements OnInit {
   ngOnInit(): void {
     this.model.companyId = this.companiesService.getMyCompanyId();
     this.getRatings();
+
+    //create chart
+
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
 
   getRatings() {
